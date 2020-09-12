1385711fb275d1333d609e235c79939c7b50d000
Kuldeep <kuldeep.panchal669@gmail.com>
2020-09-13 00:29:40 +0530

    resolved filters dynamic part

--- src/app/space-launch-programs/space-launch-programs.component.ts
+++ src/app/space-launch-programs/space-launch-programs.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 import { SpaceService } from './space.service';
 import { Router, ActivatedRoute } from '@angular/router';
+import { combineLatest } from 'rxjs';
 
 @Component({
   selector: 'app-space-launch-programs',
@@ -11,9 +12,9 @@ import { Router, ActivatedRoute } from '@angular/router';
 export class SpaceLaunchProgramsComponent implements OnInit {
   spacePrograms: Array<{}>;
   filter: {
-    launchYears: Array<{year: number, active: boolean}>,
-    successfulLaunch: boolean,
-    successfullLanding: boolean
+    launchYears: Array<{ year: number, active: boolean }>,
+    successfullLaunch: boolean,
+    successfullLand: boolean
   };
 
   defaultParms;
@@ -22,67 +23,100 @@ export class SpaceLaunchProgramsComponent implements OnInit {
     this.spacePrograms = [];
     this.filter = {
       launchYears: [
-        {year: 2006, active: false},
-        {year: 2007, active: false},
-        {year: 2008, active: false},
-        {year: 2009, active: false},
-        {year: 2010, active: false},
-        {year: 2011, active: false},
-        {year: 2012, active: false},
-        {year: 2013, active: false},
+        { year: 2006, active: false },
+        { year: 2007, active: false },
+        { year: 2008, active: false },
+        { year: 2009, active: false },
+        { year: 2010, active: false },
+        { year: 2011, active: false },
+        { year: 2012, active: false },
+        { year: 2013, active: false },
+        { year: 2014, active: false },
+        { year: 2015, active: false },
+        { year: 2016, active: false },
       ],
-      successfulLaunch: null,
-      successfullLanding: null
+      successfullLaunch: null,
+      successfullLand: null
     };
 
+    this._activatedRoute.queryParams.subscribe((queryParams) => {
+      if (Object.keys(queryParams).length) {
+
+      }
+      console.log('[queryParams]', queryParams);
+      this.updateFilters(queryParams);
+      this.getSpacePrograms(queryParams);
+    })
   }
 
   ngOnInit() {
-    this.getSpacePrograms();
+
   }
 
-  getSpacePrograms(): void {
-    this.spaceService.getSpaceProgramsApi({ "limit": 100 })
+  getSpacePrograms(queryParams): void {
+    this.spaceService.getSpaceProgramsApi({ ...{ "limit": 100 }, ...queryParams })
       .subscribe((spacePrograms) => {
-        const launchYears = [];
-        spacePrograms.map((acc) => {
-          // console.log(acc)
-          if (launchYears.indexOf(acc['launch_year']) == -1) {
-            launchYears.push(acc['launch_year'])
-          }
-        });
-        // this.filter.launchYears = launchYears;
-        console.log(launchYears);
+        // const launchYears = [];
+        // spacePrograms.map((acc) => {
+        //   if (launchYears.indexOf(acc['launch_year']) == -1) {
+        //     launchYears.push(acc['launch_year'])
+        //   }
+        // });
+        // console.log(launchYears);
         this.spacePrograms = spacePrograms;
       });
   }
 
   navigateTo(type, param) {
 
-    if(type == 'launch_year'){
-      this.filter.launchYears = this.filter.launchYears.map((launchYear)=>{
-        launchYear.active = false;
-        if(launchYear.year == param){
-          launchYear.active = !launchYear.active;
-        }
-        return launchYear;
-      })
-    }else if(type == 'launch_success'){
-      this.filter.successfulLaunch = !this.filter.successfulLaunch;
-    }
-    else{
-      this.filter.successfullLanding = !this.filter.successfullLanding;      
-    }
+    const queryParams = { ...this._activatedRoute.snapshot.queryParams };
+    // const queryParams = {};
 
-    const currentQueryParams = this._activatedRoute.snapshot.queryParams;
-    const queryParams = {};
-
-    if (Object.keys(currentQueryParams).length) {
-      for (let key in currentQueryParams) {
-        queryParams[key] = currentQueryParams[key];
+    // if (Object.keys(currentQueryParams).length) {
+    //   for (let key in currentQueryParams) {
+    //     queryParams[key] = currentQueryParams[key];
+    //   }
+    // }
+    if (queryParams[type] != undefined) {
+      if (queryParams[type] == param+"") {
+        delete queryParams[type];
+      } else {
+        queryParams[type] = param;
       }
+    } else {
+      queryParams[type] = param;
     }
-    queryParams[type] = param;
     this._router.navigate(['space-launch-programs'], { queryParams: queryParams })
+  }
+
+  updateFilters(queryParams) {
+    let filter = {
+      launchYears: [],
+      successfullLaunch: null,
+      successfullLand: null
+    };
+    // if (queryParams['launch_year']) {
+    let launchYears = [...this.filter.launchYears];
+    launchYears = launchYears.map((launchYear) => {
+      launchYear = { ...launchYear }
+      launchYear.active = false;
+      if (launchYear.year == queryParams['launch_year']) {
+        launchYear.active = true;
+      }
+      return launchYear;
+    });
+    filter['launchYears'] = launchYears;
+    // }
+    // if (queryParams['launch_success'] != undefined) {
+    filter['successfullLaunch'] = queryParams['launch_success'] != undefined ? queryParams['launch_success'] : null;
+    // } else {
+    //   filter['successfullLaunch'] = null;
+    // }
+    // if (queryParams['land_success'] != undefined) {
+    filter['successfullLand'] = queryParams['land_success'] != undefined ? queryParams['land_success'] : null;
+    // } else {
+    //   filter['successfullLand'] = null;
+    // }
+    this.filter = filter;
   }
 }
