87be47225ea46fe649e69c347c47a79fc5c71375
Kuldeep <kuldeep.panchal669@gmail.com>
2020-09-13 10:58:40 +0530

    implemented transferstate

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,9 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule, BrowserTransferStateModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-
-// import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
-import { InMemoryDataService } from './in-memory-data.service';
-
 import { AppRoutingModule } from './app-routing.module';
-
 import { AppComponent } from './app.component';
-
 import { PLATFORM_ID, APP_ID, Inject } from '@angular/core';
 import { isPlatformBrowser } from '@angular/common';
 
@@ -20,13 +14,7 @@ import { isPlatformBrowser } from '@angular/common';
     FormsModule,
     AppRoutingModule,
     HttpClientModule,
-
-    // The HttpClientInMemoryWebApiModule module intercepts HTTP requests
-    // and returns simulated server responses.
-    // Remove it when a real server is ready to receive requests.
-    // HttpClientInMemoryWebApiModule.forRoot(
-    //   InMemoryDataService, { dataEncapsulation: false }
-    // )
+    BrowserTransferStateModule
   ],
   declarations: [
     AppComponent,
--- src/app/space-launch-programs/space-launch-programs.component.ts
+++ src/app/space-launch-programs/space-launch-programs.component.ts
@@ -1,96 +1,122 @@
 import { Component, OnInit, PLATFORM_ID, Inject } from '@angular/core';
 import { SpaceService } from './space.service';
 import { Router, ActivatedRoute } from '@angular/router';
-import { TransferState } from '@angular/platform-browser';
+import { TransferState, makeStateKey } from '@angular/platform-browser';
 import { isPlatformServer, isPlatformBrowser } from '@angular/common';
+import { Observable, of } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
+const SP: any = makeStateKey<{}>("SP"); // SP : Space Programs
 @Component({
-  selector: 'app-space-launch-programs',
-  templateUrl: './space-launch-programs.component.html',
-  styleUrls: ['./space-launch-programs.component.css']
+    selector: 'app-space-launch-programs',
+    templateUrl: './space-launch-programs.component.html',
+    styleUrls: ['./space-launch-programs.component.css']
 })
 export class SpaceLaunchProgramsComponent implements OnInit {
-  spacePrograms: Array<{}>;
-  filter: {
-    launchYears: Array<{ year: number, active: boolean }>,
-    successfullLaunch: boolean,
-    successfullLand: boolean
-  };
-  defaultParms;
-  isBrowser: boolean;
-  isServer: boolean;
-
-  constructor(@Inject(PLATFORM_ID) platformId, private _tState: TransferState, private _activatedRoute: ActivatedRoute, private _router: Router, private spaceService: SpaceService) {
-    this.isServer = isPlatformServer(platformId);
-    this.isBrowser = isPlatformBrowser(platformId);
-    this.spacePrograms = [];
-    this.filter = {
-      launchYears: [
-        { year: 2006, active: false },
-        { year: 2007, active: false },
-        { year: 2008, active: false },
-        { year: 2009, active: false },
-        { year: 2010, active: false },
-        { year: 2011, active: false },
-        { year: 2012, active: false },
-        { year: 2013, active: false },
-        { year: 2014, active: false },
-        { year: 2015, active: false },
-        { year: 2016, active: false },
-      ],
-      successfullLaunch: null,
-      successfullLand: null
+    spacePrograms: Array<{}>;
+    filter: {
+        launchYears: Array<{ year: number, active: boolean }>,
+        successfullLaunch: boolean,
+        successfullLand: boolean
     };
+    isBrowser: boolean;
+    isServer: boolean;
 
-    this._activatedRoute.queryParams.subscribe((queryParams) => {
-      this.filter = this.updateFilters(queryParams);
-      this.getSpacePrograms(queryParams);
-    })
-  }
+    constructor(@Inject(PLATFORM_ID) platformId, private _tState: TransferState, private _activatedRoute: ActivatedRoute, private _router: Router, private spaceService: SpaceService) {
+        this.isServer = isPlatformServer(platformId);
+        this.isBrowser = isPlatformBrowser(platformId);
+        this.spacePrograms = [];
+        this.filter = {
+            launchYears: [
+                { year: 2006, active: false },
+                { year: 2007, active: false },
+                { year: 2008, active: false },
+                { year: 2009, active: false },
+                { year: 2010, active: false },
+                { year: 2011, active: false },
+                { year: 2012, active: false },
+                { year: 2013, active: false },
+                { year: 2014, active: false },
+                { year: 2015, active: false },
+                { year: 2016, active: false },
+            ],
+            successfullLaunch: null,
+            successfullLand: null
+        };
 
-  ngOnInit() {
-
-  }
-
-  getSpacePrograms(queryParams): void {
-    this.spaceService.getSpaceProgramsApi({ ...{ "limit": 100 }, ...queryParams })
-      .subscribe((spacePrograms) => {
-        this.spacePrograms = spacePrograms;
-      });
-  }
-
-  navigateTo(type, param) {
-    const queryParams = { ...this._activatedRoute.snapshot.queryParams };
-    if (queryParams[type] != undefined) {
-      if (queryParams[type] == param + "") {
-        delete queryParams[type];
-      } else {
-        queryParams[type] = param;
-      }
-    } else {
-      queryParams[type] = param;
+        this._activatedRoute.queryParams.subscribe((queryParams) => {
+            this.filter = this.updateFilters(queryParams);
+            if (this._tState.hasKey(SP)) {
+                this._tState.get(SP, []);
+            } else {
+                this.getSpacePrograms(queryParams)
+                    .pipe(
+                        catchError((err) => {
+                            return of([]);
+                        })
+                    )
+                    .subscribe((spacePrograms) => {
+                        if (this.isServer) {
+                            this._tState.set(SP, spacePrograms);
+                        }
+                        this.initilizeData(spacePrograms);
+                    });
+            }
+        })
     }
-    this._router.navigate(['space-launch-programs'], { queryParams: queryParams })
-  }
 
-  updateFilters(queryParams) {
-    let filter = {
-      launchYears: [],
-      successfullLaunch: null,
-      successfullLand: null
-    };
-    let launchYears = [...this.filter.launchYears];
-    launchYears = launchYears.map((launchYear) => {
-      launchYear = { ...launchYear }
-      launchYear.active = false;
-      if (launchYear.year == queryParams['launch_year']) {
-        launchYear.active = true;
-      }
-      return launchYear;
-    });
-    filter['launchYears'] = launchYears;
-    filter['successfullLaunch'] = queryParams['launch_success'] != undefined ? queryParams['launch_success'] : null;
-    filter['successfullLand'] = queryParams['land_success'] != undefined ? queryParams['land_success'] : null;
-    return filter;
-  }
+    ngOnInit() {
+
+    }
+
+    ngAfterViewInit()
+    {
+        /*Remove key set on server side to avoid api on dom load of frontend side*/
+        if (this.isBrowser) {
+            this._tState.remove(SP);
+        }
+    }
+
+    getSpacePrograms(queryParams): Observable<Array<{}>> {
+        return this.spaceService.getSpaceProgramsApi({ ...{ "limit": 100 }, ...queryParams });
+    }
+
+    initilizeData(spacePrograms) {
+        this.spacePrograms = spacePrograms;
+    }
+
+    navigateTo(type, param) {
+        const queryParams = { ...this._activatedRoute.snapshot.queryParams };
+        if (queryParams[type] != undefined) {
+            if (queryParams[type] == param + "") {
+                delete queryParams[type];
+            } else {
+                queryParams[type] = param;
+            }
+        } else {
+            queryParams[type] = param;
+        }
+        this._router.navigate(['space-launch-programs'], { queryParams: queryParams })
+    }
+
+    updateFilters(queryParams) {
+        let filter = {
+            launchYears: [],
+            successfullLaunch: null,
+            successfullLand: null
+        };
+        let launchYears = [...this.filter.launchYears];
+        launchYears = launchYears.map((launchYear) => {
+            launchYear = { ...launchYear }
+            launchYear.active = false;
+            if (launchYear.year == queryParams['launch_year']) {
+                launchYear.active = true;
+            }
+            return launchYear;
+        });
+        filter['launchYears'] = launchYears;
+        filter['successfullLaunch'] = queryParams['launch_success'] != undefined ? queryParams['launch_success'] : null;
+        filter['successfullLand'] = queryParams['land_success'] != undefined ? queryParams['land_success'] : null;
+        return filter;
+    }
 }
